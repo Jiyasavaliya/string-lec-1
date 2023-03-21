1a83db0d4b1800a2a8164e5e343153e56ce20be6
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-03-21 12:35:37 +0530

    Add files via upload

---
+++ string in lowercase.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	
+	char a[] = "HELLO";
+    
+    printf("string lowercase : %s \n" ,strlwr(a));
+	
+	return 0;
+}
---
+++ string in uppercase.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	
+	char a[] = "hello";
+    
+    printf("string uppercase : %s \n" ,strupr(a));
+	
+	return 0;
+}
